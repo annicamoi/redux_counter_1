6fa5de8ca19eb2fdd214dc251a1ce1207ccc83c9
Moilanen Annika <s2100141@edu.bc.fi>
2021-08-17 13:37:23 +0300

    added buttons

--- src/container/Counter.js
+++ src/container/Counter.js
@@ -6,10 +6,26 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <div className="counter_text">Your score: {this.props.ctr}</div>
-        <button onClick={this.props.onIncCounter}>Increase one</button>
-        <button onClick={this.props.onDecCounter}>Decrease one</button>
-        <button onClick={this.props.resetCounter}>Reset</button>
+        <div>
+          <div className="counter_text">Your score: {this.props.ctr}</div>
+          <button onClick={this.props.onIncCounter}>Increase one</button>
+          <button onClick={this.props.onDecCounter}>Decrease one</button>
+          <button onClick={this.props.onAddFive}>Add five</button>
+          <button onClick={this.props.resetCounter}>Reset</button>
+        </div>
+        <button onClick={this.props.onStoreResult}>Store the results</button>
+        <div>
+          <ul>
+            {this.props.storedResults.map((item) => (
+              <li
+                key={item.id}
+                onCLick={() => this.props.onDeleteResult(item.id)}
+              >
+                {item.value}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
@@ -18,6 +34,7 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
   return {
     ctr: state.counter,
+    storedResults: state.results,
   };
 };
 
@@ -25,7 +42,11 @@ const mapDispatchToProps = (dispatch) => {
   return {
     onIncCounter: () => dispatch({ type: actionType.INCREMENT }),
     onDecCounter: () => dispatch({ type: actionType.DECREMENT }),
+    onAddFive: () => dispatch({ type: actionType.ADD, value: 5 }),
     resetCounter: () => dispatch({ type: actionType.RESET }),
+    onStoreResult: () => dispatch({ type: actionType.STORE_RESULT }),
+    onDeleteResult: (id) =>
+      dispatch({ type: actionType.DELETE_RESULT, item: id }),
   };
 };
 
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -9,6 +9,21 @@ const reducer = (state = initialState, action) => {
       };
     case actionType.DECREMENT:
       return { ...state, counter: state.counter > 0 ? state.counter - 1 : 0 };
+    case actionType.ADD:
+      return { ...state, counter: state.counter + action.value };
+    case actionType.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: state.counter }),
+      };
+    case actionType.DELETE_RESULT:
+      const updateArray = state.results.filter(
+        (item) => item.id !== action.item
+      );
+      return {
+        ...state,
+        results: updateArray,
+      };
     case actionType.RESET:
       return {
         ...state,
@@ -20,6 +35,16 @@ const reducer = (state = initialState, action) => {
 
 const initialState = {
   counter: 0,
+  results: [
+    {
+      id: 1,
+      value: "testing",
+    },
+    {
+      id: 2,
+      value: "stupid data",
+    },
+  ],
 };
 
 export default reducer;
